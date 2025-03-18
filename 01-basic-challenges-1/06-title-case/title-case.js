ee55e96aa193d225633fc2dad9055b58f5c73588
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-03-18 11:20:52 -0300

    challenge 06

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,13 @@
-function titleCase() {}
+function titleCase(str) {
+  const words = str.toLowerCase().split(" ");
+  const captializedWords = [];
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    captializedWords.push(word[0].toUpperCase() + word.substring(1, word.length))
+  }
+
+  return captializedWords.join(" ");
+}
 
 module.exports = titleCase;
