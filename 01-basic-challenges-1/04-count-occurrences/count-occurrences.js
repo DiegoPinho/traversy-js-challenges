d605fd5fe009f84b7131094f6cc6e402a51de048
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-03-18 10:52:02 -0300

    challenge 04

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,12 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let ocurrences = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      ocurrences = ocurrences + 1;
+    }
+  }
+
+  return ocurrences;
+}
 
 module.exports = countOccurrences;
