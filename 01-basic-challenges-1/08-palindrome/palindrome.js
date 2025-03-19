0903c3765d8ac15aceff8fe84b13c39493bc7e99
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-03-19 19:47:40 -0300

    challenge 08

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,12 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, "").replace(" ", "");
+
+  let reverseStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr = reverseStr + str[i];
+  }
+
+  return str === reverseStr;
+}
 
 module.exports = isPalindrome;
