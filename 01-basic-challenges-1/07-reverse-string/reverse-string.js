5e4ab79e3914a7dfbe9018fe4483e2cf5d4bbe94
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-03-19 19:31:55 -0300

    challenge 07

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,10 @@
-function reverseString() {}
+function reverseString(str) {
+  let reverseStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr = reverseStr + str[i];
+  }
+
+  return reverseStr;
+}
 
 module.exports = reverseString;
