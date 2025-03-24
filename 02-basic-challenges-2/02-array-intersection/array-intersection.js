d82cb017b850918523157db7f3d29dcfc223e05e
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-03-24 20:08:44 -0300

    challenge 02 - 02

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,15 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const commonValues = [];
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] == arr2[j]) {
+        commonValues.push(arr1[i]);
+        break;
+      }
+    }
+  }
+
+  return commonValues;
+}
 
 module.exports = arrayIntersection;
