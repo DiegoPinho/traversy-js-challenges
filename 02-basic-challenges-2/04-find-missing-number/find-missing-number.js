6aeec1d76a01ca3af382da950c998255805a8eae
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-03-25 10:16:19 -0300

    challenge 02 04

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,15 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  if (arr.length === 0) return 1;
+  if (!arr) return undefined;
+
+  const highestValue = arr.length + 1;
+  for (let i = 1; i <= highestValue; i++) {
+    if (!arr.includes(i)) {
+      return i;
+    }
+  }
+
+
+}
 
 module.exports = findMissingNumber;
