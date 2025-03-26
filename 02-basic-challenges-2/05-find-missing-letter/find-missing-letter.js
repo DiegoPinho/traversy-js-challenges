3661259c3311cf2698a8a936f7cbc35a5f34b319
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-03-26 19:41:23 -0300

    challenge 02 05

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,21 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  let length = arr.length;
+
+  if (length === 0) return "";
+
+  let expectedSum = 0;
+  for (i = arr[0].charCodeAt(); i <= arr[length - 1].charCodeAt(); i++) {
+    expectedSum = expectedSum + i;
+  }
+
+  let actualSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    actualSum = actualSum + arr[i].charCodeAt();
+  }
+
+  const missingLetterCode = expectedSum - actualSum;
+  return String.fromCharCode(missingLetterCode);
+}
+
 
 module.exports = findMissingLetter;
