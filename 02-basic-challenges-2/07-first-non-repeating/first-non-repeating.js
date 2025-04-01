7b3733346fef719c14e9f36413260548640365c2
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-04-01 11:43:42 -0300

    challenge 02 07

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,16 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  const charApparences = {};
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (!charApparences[char]) charApparences[char] = 1;
+    else charApparences[char] = charApparences[char] + 1;
+  }
+
+  for (let char in charApparences) {
+    if (charApparences[char] === 1) return char;
+  }
+
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
