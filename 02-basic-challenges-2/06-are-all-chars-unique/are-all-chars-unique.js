7cbadb5fccb49777de75c4a71af4dd9f979ccf13
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-04-01 11:21:18 -0300

    challenge 02 06

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,16 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  if (!str) return true;
+
+  const uniqueChars = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (uniqueChars.indexOf(char) > 0) return false;
+    else uniqueChars.push(char);
+  }
+
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
