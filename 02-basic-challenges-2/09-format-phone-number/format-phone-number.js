5da23e0aa3f5cfda5690443aaddd7dfab2a556ea
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-04-08 11:06:07 -0300

    challenge 02 - 09

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,20 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numbers) {
+  let ddd = "";
+  let firstNumbers = "";
+  let lastNumbers = "";
+
+  for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    if (i < 3) {
+      ddd = ddd + number;
+    } else if (i >= 3 && i <= 5) {
+      firstNumbers = firstNumbers + number;
+    } else {
+      lastNumbers = lastNumbers + number;
+    }
+  }
+
+  return `(${ddd}) ${firstNumbers}-${lastNumbers}`;
+}
 
 module.exports = formatPhoneNumber;
