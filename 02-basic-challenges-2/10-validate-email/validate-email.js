94c71dc6772973d84f6b7b1cc7b7d2920bcdc148
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-04-08 11:32:58 -0300

    challenge 02 10

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,11 @@
-function validateEmail() {}
+function validateEmail(email) {
+  const index = email.indexOf("@");
+  if (index > 0) {
+    const domain = email.substring(index);
+    return domain.includes(".");
+  }
+
+  return false;
+}
 
 module.exports = validateEmail;
