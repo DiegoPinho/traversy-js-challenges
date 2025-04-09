5217f19ba81cab58dd7f3df0ef87ff6935c5bb75
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-04-09 10:26:37 -0300

    challenge 03 02

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,14 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  let sumOfEvenSquares = 0;
+  numbers.forEach(number => {
+    const isEven = number % 2 === 0;
+    if (isEven) {
+      const square = number * number;
+      sumOfEvenSquares = sumOfEvenSquares + square; // ** 2
+    }
+  });
+
+  return sumOfEvenSquares;
+}
 
 module.exports = sumOfEvenSquares;
