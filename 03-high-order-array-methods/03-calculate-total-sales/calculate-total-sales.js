4f06fea4f0f2035ed66bfc983783119c3f546d72
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-04-09 10:42:05 -0300

    challenge 03 03

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,12 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  let total = 0;
+
+  products.forEach(product => {
+    total = total + (product.price * product.quantity);
+  });
+
+  const taxes = (total * taxRate) / 100;
+  return total + taxes;
+}
 
 module.exports = calculateTotalSalesWithTax;
